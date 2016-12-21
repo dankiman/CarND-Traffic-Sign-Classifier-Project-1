77fd482fb10e79d81a2fcb34c38ea51cc968a5e8
wolfapple <illumine03@naver.com>
2016-12-21 18:45:20 +0900

    ...

--- conv.py
+++ conv.py
@@ -1,4 +1,5 @@
 import pickle
+from sklearn.model_selection import train_test_split
 
 training_file = "data/train.p"
 testing_file = "data/test.p"
@@ -8,19 +9,19 @@ with open(training_file, mode='rb') as f:
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
     
-X_train, y_train = train['features'], train['labels']
+X_train, X_validation, y_train, y_validation = train_test_split(train['features'], train['labels'], test_size=0.2)
 X_test, y_test = test['features'], test['labels']
 
 import tensorflow as tf
 from sklearn.utils import shuffle
 from tensorflow.contrib.layers import flatten
 
-EPOCHS = 20
+EPOCHS = 30
 BATCH_SIZE = 50
 DROPOUT = 0.75
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.truncated_normal(shape, mean=0, stddev=0.1)
     return tf.Variable(initial)
 
 def bias_variable(shape):
@@ -35,7 +36,7 @@ def conv2d(x, W, b, strides=1):
 def maxpool2d(x, k=2):
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
     
-def conv_net2(x, dropout):
+def conv_net(x, dropout):
     # Convolution Layer 1. 32x32x3 -> 32x32x6
     conv1_W = weight_variable([5, 5, 3, 6])
     conv1_b = bias_variable([6])
@@ -43,6 +44,7 @@ def conv_net2(x, dropout):
     
     # Pooling Layer 1. 32x32x6 -> 16x16x6
     conv1 = maxpool2d(conv1)
+    # conv1 = tf.nn.dropout(conv1, dropout)
     
     # Convolution Layer 2. 16x16x6 -> 16x16x16
     conv2_W = weight_variable([5, 5, 6, 16])
@@ -51,22 +53,16 @@ def conv_net2(x, dropout):
     
     # Pooling Layer 2. 16x16x16 -> 8x8x16
     conv2 = maxpool2d(conv2)
-
-    # Convolution Layer 3. 16x16x6 -> 16x16x16
-    conv3_W = weight_variable([5, 5, 16, 32])
-    conv3_b = bias_variable([32])
-    conv3 = conv2d(conv2, conv3_W, conv3_b)
+    # conv2 = tf.nn.dropout(conv2, dropout)
     
-    # Pooling Layer 3. 16x16x16 -> 8x8x16
-    conv3 = maxpool2d(conv3)
-    
-    # Fully Connected Layer 1. 8x8x16 -> 120
-    fc1_W = weight_variable([4*4*32, 120])
+    # Fully Connected Layer 1. 4x4x32 -> 120
+    fc1_W = weight_variable([8*8*16, 120])
     fc1_b = bias_variable([120])
-    fc1 = tf.reshape(conv3, [-1, 4*4*32])
+    fc1 = tf.reshape(conv2, [-1, 8*8*16])
     fc1 = tf.nn.relu(tf.matmul(fc1, fc1_W) + fc1_b)
+    fc1 = tf.nn.dropout(fc1, dropout)
 
-    # Fully Connected Layer 2. 8x8x16 -> 120
+    # Fully Connected Layer 2. 120 -> 84
     fc2_W = weight_variable([120, 84])
     fc2_b = bias_variable([84])
     fc2 = tf.nn.relu(tf.matmul(fc1, fc2_W) + fc2_b)
@@ -74,71 +70,18 @@ def conv_net2(x, dropout):
     # Apply Dropout
     fc2 = tf.nn.dropout(fc2, dropout)
     
-    # Fully Connected Layer 3. 120 -> 43
+    # Fully Connected Layer 3. 84 -> 43
     fc3_W = weight_variable([84, 43])
     fc3_b = bias_variable([43])
     fc3 = tf.matmul(fc2, fc3_W) + fc3_b
     return fc3
-
-def conv_net(x, dropout):
-    # Reshape from 2D to 4D. This prepares the data for
-    # convolutional and pooling layers.
-    # x = tf.reshape(x, (-1, 32, 32, 3))
-    # Pad 0s to 32x32. Centers the digit further.
-    # Add 2 rows/columns on each side for height and width dimensions.
-    x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], mode="CONSTANT")
-    
-    # Hyperparameters
-    mu = 0
-    sigma = 0.1
-    
-    # SOLUTION: Convolution Layer 1. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = mu, stddev = sigma))
-    conv1_b = tf.Variable(tf.zeros(6))
-    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
-
-    # SOLUTION: Activation 1.
-    conv1 = tf.nn.relu(conv1)
-
-    # SOLUTION: Pooling Layer 1. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Convolution Layer 2. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
-    conv2_b = tf.Variable(tf.zeros(16))
-    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-    
-    # SOLUTION: Activation 2.
-    conv2 = tf.nn.relu(conv2)
-
-    # SOLUTION: Pooling Layer 2. Input = 10x10x16. Output = 5x5x16.
-    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Flatten Layer.
-    fc1 = flatten(conv2)
-    fc1_shape = (fc1.get_shape().as_list()[-1], 120)
-    
-    # SOLUTION: Fully Connected Layer 1. Input = 5x5x16. Output = 120.
-    fc1_W     = tf.Variable(tf.truncated_normal(shape=(fc1_shape), mean = mu, stddev = sigma))
-    fc1_b     = tf.Variable(tf.zeros(120))
-    fc1       = tf.matmul(fc1, fc1_W) + fc1_b
-    
-    # SOLUTION: Activation 3.
-    fc1 = tf.nn.relu(fc1)
-
-    # SOLUTION: Fully Connected Layer 2. Input = 120. Output = 10.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 43), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(43))
-    logits = tf.matmul(fc1, fc2_W) + fc2_b
-    
-    return logits
 
 x = tf.placeholder(tf.float32, (None, 32, 32, 3))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, 43)
 keep_prob = tf.placeholder(tf.float32)
 
-logits = conv_net2(x, keep_prob)
+logits = conv_net(x, keep_prob)
 loss_operation = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y))
 optimizer = tf.train.AdamOptimizer()
 training_operation = optimizer.minimize(loss_operation)
@@ -169,7 +112,7 @@ with tf.Session() as sess:
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
             loss = sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: DROPOUT})
             
-        validation_loss, validation_accuracy = evaluate(X_train, y_train, 1.)
+        validation_loss, validation_accuracy = evaluate(X_validation, y_validation, 1.)
         print("EPOCH {} ...".format(i+1))
         print("Validation Loss     = {:.3f}".format(validation_loss))
         print("Validation Accuracy = {:.3f}".format(validation_accuracy))
